5e273cb496d375e16f55675bb678c99bbfaf5aa3
liuzhouliang <525688067@qq.com>
2017-09-14 23:04:40 +0800

    ii

--- RxJava/src/main/java/com/example/liuzhouliang/demo4/MainActivity.java
+++ RxJava/src/main/java/com/example/liuzhouliang/demo4/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 /**
  * RXJAVA 一个在 Java VM 上使用可观测的序列来组成异步的、基于事件的程序的库
@@ -112,20 +115,20 @@ public class MainActivity extends AppCompatActivity {
         Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
-                Log.e(TAG,"Subscriber==onCompleted");
+                Log.e(TAG, "Subscriber==onCompleted");
                 mComplete.setText("Subscriber===onCompleted");
 
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG,"Subscriber==onStart");
+                Log.e(TAG, "Subscriber==onStart");
                 mComplete.setText("Subscriber===onError");
             }
 
             @Override
             public void onNext(String o) {
-                Log.e(TAG,"Subscriber==onNext");
+                Log.e(TAG, "Subscriber==onNext");
                 content.setText(o);
 
             }
@@ -133,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStart() {
                 super.onStart();
-                Log.e(TAG,"Subscriber==onStart");
+                Log.e(TAG, "Subscriber==onStart");
             }
         };
         //使用Observable.create()创建被观察者============================2
@@ -152,13 +155,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *Subscriber 实现 Observer 接口
-     *
-     *二者区别
-     *
+     * Subscriber 实现 Observer 接口
+     * <p>
+     * 二者区别
+     * <p>
      * onStart(): 这是Subscriber增加的方法。它会在 subscribe 刚开始，而事件还未发送之前被调用，可以用于做一些准备工作，例如数据的清零或重置。这是一个可选方法，默认情况下它的实现为空。需要注意的是，如果对准备工作的线程有要求
-     *
+     * <p>
      * unsubscribe(): 这是Subscriber所实现的另一个接口Subscription
      * 的方法，用于取消订阅。在这个方法被调用后，Subscriber将不再接收事件。一般在这个方法调用前，可以使用isUnsubscribed()先判断一下状态。unsubscribe()这个方法很重要，因为在subscribe()之后，Observable会持有Subscriber的引用，这个引用如果不能及时被释放，将有内存泄露的风险。所以最好保持一个原则：要在不再使用的时候尽快在合适的地方（例如onPause()onStop()等方法中）调用unsubscribe()来解除引用关系，以避免内存泄露的发生。
      */
+
+    /**
+     * Action的使用======================
+     * <p>
+     * 什么是Action
+     * Action是RxJava 的一个接口，常用的有Action0和Action1
+     * <p>
+     * Action0： 它只有一个方法 call()，这个方法是无参无返回值的；由于 onCompleted() 方法也是无参无返回值的，因此 Action0 可以被当成一个包装对象，将 onCompleted() 的内容打包起来将自己作为一个参数传入 subscribe() 以实现不完整定义的回调。
+     * Ation1：它同样只有一个方法 call(T param)，这个方法也无返回值，但有一个参数；与 Action0 同理，由于 onNext(T obj) 和 onError(Throwable error) 也是单参数无返回值的，因此 Action1 可以将 onNext(obj)和 onError(error) 打包起来传入 subscribe() 以实现不完整定义的回调
+     *
+     * @param view
+     */
+    public void test3(View view) {
+        Observable.just("Hello", "World")
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                        content.setText(s);
+                    }
+                });
+
+
+    }
+
+    /**
+     *
+     * 定义三个对象，分别打包onNext(obj)、onError(error) 、onCompleted()。
+     * @param view
+     */
+    public void test4(View view) {
+
+
+//        Observable observable = Observable.just("Hello", "World");
+        //处理onNext()中的内容
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.e(TAG, s);
+            }
+        };
+        //处理onError()中的内容
+        Action1<Throwable> onErrorAction = new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e(TAG, "onErrorAction");
+            }
+        };
+        //处理onCompleted()中的内容
+        Action0 onCompletedAction = new Action0() {
+            @Override
+            public void call() {
+                Log.e(TAG, "onCompletedAction");
+
+            }
+        };
+        //使用 onNextAction 来定义 onNext()
+        Observable.just("Hello", "World").subscribe(onNextAction);
+        //使用 onNextAction 和 onErrorAction 来定义 onNext() 和 onError()
+        Observable.just("Hello", "World").subscribe(onNextAction, onErrorAction);
+        //使用 onNextAction、 onErrorAction 和 onCompletedAction 来定义 onNext()、 onError() 和 onCompleted()
+        Observable.just("Hello", "World").subscribe(onNextAction, onErrorAction, onCompletedAction);
+
+    }
 }
